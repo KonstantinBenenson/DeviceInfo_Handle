c229f490d632574f2bcc0464f9c493a1482002ae
MarkellBeast <km.benenson@gmail.com>
2022-07-07 00:01:27 +0300

    Добавьте файлы проекта.

---
+++ DeviceInfo_Handle/Extensions/DataFilter.cs
@@ -0,0 +1,42 @@
+﻿using DeviceInfo_Handle.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceInfo_Handle.Extensions
+{
+    public static class DataFilter
+    {
+        public static List<DeviceInfo> FindData(this List<DeviceInfo> list)
+        {
+            List<DeviceInfo> result = new List<DeviceInfo>();
+            for (int i = 0; i < list.Count(); i++)
+            {
+                List<DeviceInfo> temp = new List<DeviceInfo>();
+                int number = int.Parse(list[i].Brigade.Code);
+                for (int y = i+1; y < list.Count()-1; y++)
+                {
+                    if(int.Parse(list[y].Brigade.Code) == number)
+                    {
+                        if(!temp.Contains(list[i]))
+                            temp.Add(list[i]);
+
+                        temp.Add(list[y]);
+                    }                        
+                }
+                for (int j = 0; j < temp.Count()-1; j++)
+                {
+                    if (temp[j].Device.IsOnline)
+                    {
+                        result.AddRange(temp);
+                        break;
+                    }
+                    continue;
+                }
+            }
+           return result;
+        }
+    }    
+}
---
+++ DeviceInfo_Handle/Program.cs
@@ -0,0 +1,17 @@
+﻿using DeviceInfo_Handle.Models.DTOs;
+using Newtonsoft.Json;
+using DeviceInfo_Handle.Extensions;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = "C:\\Users\\markell\\Desktop\\Тестовое B-1336\\Задание2\\Devices.json";
+        using (StreamReader sr = new StreamReader(path))
+        {
+            var text = sr.ReadToEnd();
+            var result = JsonConvert.DeserializeObject<List<DeviceInfo>>(text).FindData();
+
+            var newData = JsonConvert.SerializeObject<Conflict>(result);
+        }
+    }
+}
