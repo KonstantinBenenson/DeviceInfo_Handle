b5d7ae2f688aebfe14d861fa2d810557a6ac9947
MarkellBeast <km.benenson@gmail.com>
2022-07-12 13:54:30 +0300

    v1.2 Оптимизированы методы расширения: все сведено к методу ToConflicts, остальной функционал разбит на приватные методы

--- DeviceInfo_Handle/Extensions/DataFilter.cs
+++ DeviceInfo_Handle/Extensions/DataFilter.cs
@@ -9,56 +9,63 @@ namespace DeviceInfo_Handle.Extensions
 {
     public static class DataFilter
     {
-        public static List<DeviceInfo> FindData(this List<DeviceInfo> list)
-        {
-            List<DeviceInfo> result = new List<DeviceInfo>();
-            for (int i = 0; i < list.Count(); i++)
-            {
-                List<DeviceInfo> temp = new List<DeviceInfo>();
-                int number = int.Parse(list[i].Brigade.Code);
-                for (int y = i + 1; y < list.Count() - 1; y++)
-                {
-                    if (int.Parse(list[y].Brigade.Code) == number)
-                    {
-                        if (!temp.Contains(list[i]))
-                            temp.Add(list[i]);
-
-                        temp.Add(list[y]);
-                    }
-                }
-                for (int j = 0; j < temp.Count() - 1; j++)
-                {
-                    if (temp[j].Device.IsOnline)
-                    {
-                        result.AddRange(temp);
-                        break;
-                    }
-                    continue;
-                }
-            }
-            return result;
-        }
         public static List<Conflict> ToConflicts(this List<DeviceInfo> list)
         {
+            list.FindByBrigadeCode();
+
             List<Conflict> conflicts = new();
             for (int i = 0; i < list.Count; i++)
             {
                 Conflict conflict = new Conflict
                 {
                     BrigadeCode = list[i].Brigade.Code
-                };      
+                };
                 List<string> serialNumbers = new();
                 for (int y = 0; y < list.Count; y++)
                 {
-                    if(list[y].Brigade.Code == list[i].Brigade.Code)
+                    if (list[y].Brigade.Code == list[i].Brigade.Code)
                         serialNumbers.Add(list[y].Device.SerialNumber);
-                }                
-                if(serialNumbers.Count > 0)
+                }
+                if (serialNumbers.Count > 0)
                     conflict.DevicesSerials = serialNumbers.ToArray();
 
                 conflicts.Add(conflict);
             }
             return conflicts;
         }
+
+        private static void FindByBrigadeCode(this List<DeviceInfo> list)
+        {
+            List<DeviceInfo> result = new List<DeviceInfo>();
+            for (int i = 0; i < list.Count(); i++)
+            {
+                List<DeviceInfo> temp = new List<DeviceInfo>();
+                var code = list[i].Brigade.Code;
+                for (int y = i + 1; y < list.Count() - 1; y++)
+                {
+                    if (list[y].Brigade.Code == code)
+                    {
+                        if (!temp.Contains(list[i]))
+                            temp.Add(list[i]);
+
+                        temp.Add(list[y]);
+                    }
+                    result.AddIfIsOnline(temp);
+                }                
+            }
+        }
+
+        private static void AddIfIsOnline(this List<DeviceInfo> result, List<DeviceInfo> temp)
+        {
+            for (int j = 0; j < temp.Count() - 1; j++)
+            {
+                if (temp[j].Device.IsOnline)
+                {
+                    result.AddRange(temp);
+                    break;
+                }
+                continue;
+            }
+        }        
     }
 }
--- DeviceInfo_Handle/Program.cs
+++ DeviceInfo_Handle/Program.cs
@@ -9,7 +9,7 @@ class Program
         using (StreamReader sr = new StreamReader(path))
         {            
             var text = sr.ReadToEnd();
-            var result = JsonConvert.DeserializeObject<List<DeviceInfo>>(text).FindData().ToConflicts();
+            var result = JsonConvert.DeserializeObject<List<DeviceInfo>>(text).ToConflicts();
 
             var newData = JsonConvert.SerializeObject(result, Formatting.Indented);
             File.WriteAllText("C:\\Users\\markell\\Desktop\\Тестовое B-1336\\Задание2\\Conflicts.json", newData);
